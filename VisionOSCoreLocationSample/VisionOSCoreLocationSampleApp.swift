92c8b356a20b2b1021581b91b927f1dc2777e5ab
tokufxug <tokufxug@gmail.com>
2024-01-04 22:01:17 +0900

    Initial Commit

---
+++ VisionOSCoreLocationSample/VisionOSCoreLocationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSCoreLocationSampleApp.swift
+//  VisionOSCoreLocationSample
+//
+//  Created by Sadao Tokuyama on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSCoreLocationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
