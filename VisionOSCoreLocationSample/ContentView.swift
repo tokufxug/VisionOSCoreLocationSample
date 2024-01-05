11d16a3efea794b90c4cb28deb2ad0b2557f66ed
tokufxug <tokufxug@gmail.com>
2024-01-05 13:31:43 +0900

    commit

--- VisionOSCoreLocationSample/ContentView.swift
+++ VisionOSCoreLocationSample/ContentView.swift
@@ -6,18 +6,43 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
+import CoreLocation
 
 struct ContentView: View {
+
+    private let manager: CLLocationManager = CLLocationManager()
+    @State var locationData: String = ""
+    
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+            Button("Start Location") {
+                Task {
+                   await startLocationUpdates()
+                }
+            }.font(.extraLargeTitle)
+            Text(locationData).font(.extraLargeTitle)
         }
         .padding()
+    }
+    
+    func startLocationUpdates() async {
+        if manager.authorizationStatus == .notDetermined {
+            manager.requestWhenInUseAuthorization()
+        }
+        do {
+            let updates = CLLocationUpdate.liveUpdates()
+            for try await update in updates {
+                if let location = update.location {
+                    locationData = "\(location)"
+                    print(locationData)
+                }
+                if update.isStationary {
+                    break
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
 }
 
